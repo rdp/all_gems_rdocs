863cacb739978564c52a09757670dd84d12e7fcb
Roger Pack <rogerpack2005@gmail.com>
2009-06-20 17:38:20 -0600

    ilab friendlier

--- sync_all_gems.rb
+++ sync_all_gems.rb
@@ -1,9 +1,18 @@
+# this does install and generate rdocs
+
+if `hostname`.strip == "ilab1"
+    on_ilab = true
+end
+
+ENV['GEM_PATH'] = '/home/rdp/dev/linode/installs/mbari_gembox_187/lib/ruby/gems/1.8' if on_ilab
+
 generate_rdoc = ARGV[0] == '--generate_rdoc'
 
+
 if generate_rdoc
-  all = `./gem list`
+  all = `gem list`
 else
-  all = `./gem list -r` # we don't do gems.github.com yet
+  all = `gem list -r` # we don't do gems.github.com yet
 end
 
 
@@ -18,11 +27,11 @@ if generate_rdoc
  ARGV=['rdoc', name, '--no-ri']
  p 'ARGV is', ARGV
  Process.wait fork {
-  load 'gem'
+  require './gem' # install rdocs appropo
  } 
  puts 'here--done with gem' + name
 else  
- command = "./gem install #{name} --version=#{versions.sort.last} --no-rdoc --no-ri"
+ command = "gem install #{name} --version=#{versions.sort.last} --no-rdoc --no-ri"
  # just in case system(command)
 end
 
