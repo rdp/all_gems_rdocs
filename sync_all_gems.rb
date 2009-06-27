eadca48f9cf934fbd8cb5d8b5a00a2d6e435881b
Roger Pack <rogerpack2005@gmail.com>
2009-06-27 17:53:21 -0600

    make it more friendly for hashes

--- sync_all_gems.rb
+++ sync_all_gems.rb
@@ -10,9 +10,6 @@ raise unless ARGV[0] if $0 == __FILE__
 ENV['GEM_PATH'] = '/home/rdp/dev/linode/installs/mbari_gembox_187/lib/ruby/gems/1.8'
 
 bin_dir = '/home/rdp/dev/linode/installs/mbari_gembox_187/bin'
-generate_rdoc = ARGV[0] == '--generate_rdoc'
-
-require 'pp'
 
 =begin
 doctest: parses right
@@ -39,22 +36,28 @@ def get_gems this_big_string
    return all_gems
 end
 
-if generate_rdoc
-   require 'rubygems' # pre load it, so fork works and doesn't have to reload rubygems which takes forever
-   all = `gem list -l`
-   parsed = get_gems all
+require 'hash_set_operators'
 
-   parsed.each{|name, version|
-      ARGV=['rdoc', name, '--no-ri']
-      p 'ARGV is', ARGV
-      Process.wait fork {
-         load "#{bin_dir}/gem" # install rdocs appropo
+if $0 == __FILE__
+   if ARGV[0] == '--generate_rdoc'
+      require 'rubygems' # pre load it, so fork works and doesn't have to reload rubygems which takes forever
+      all = `gem list -l`
+      parsed = get_gems all
+
+      parsed.each{|name, version|
+         ARGV=['rdoc', name, '--no-ri']
+         p 'ARGV is', ARGV
+         Process.wait fork {
+            load "#{bin_dir}/gem" # install rdocs appropo
+         }
+         puts 'here--done with gem' + name
       }
-      puts 'here--done with gem' + name
-   }
-else
-   all = `gem list -r` # we don't do gems.github.com yet
-   # gem list -r --source http://gems.github.com
-   # command = "gem install #{name} --version=#{versions.sort.last} --no-rdoc --no-ri"
-   # just in case system(command)
-end if $0 == __FILE__
+   elsif ARGV[0] == '--install-missing'
+      all = `gem list -r` # we don't do gems.github.com yet
+      local = `gem list -l`
+      # gem list -r --source http://gems.github.com
+      # command = "gem install #{name} --version=#{versions.sort.last} --no-rdoc --no-ri"
+      # just in case system(command)
+   end
+
+end
