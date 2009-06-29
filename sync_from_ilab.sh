2a3654ed104c5f266009c35b198a4d1d74b0b371
Roger Pack <rogerpack2005@gmail.com>
2009-06-29 16:20:42 -0600

    restart mod rails, too

--- sync_from_ilab.sh
+++ sync_from_ilab.sh
@@ -1,3 +1,4 @@
 # this does docs, too
 rsync -r /home/rdp/dev/linode/installs/mbari_gembox_187/lib/ruby/gems/*  rdp@faithpromotingstories.org:~/installs/mbari_gembox_187/lib/ruby/gems
 ssh rdp@faithpromotingstories.org "touch ~/prod/gembox/tmp/restart.txt"
+curl http://allgems.faithpromotingstories.org/gems > /dev/null # re-read it
